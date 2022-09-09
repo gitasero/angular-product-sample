6b75b40d76a07f68997f1d9519aafcb659685cb9
gitasero <111991156+gitasero@users.noreply.github.com>
2022-09-09 09:54:06 +0200

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartService } from './cart.service';
 
 @NgModule({
   imports: [
@@ -28,6 +29,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   ],
   bootstrap: [
     AppComponent
+  ],
+  providers: [
+    CartService
   ]
 })
 export class AppModule { }

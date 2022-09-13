3cd8d81844e511322c7e1907e333e10c3c50e400
gitasero <111991156+gitasero@users.noreply.github.com>
2022-09-13 15:06:25 +0200

    4 files modified CartService e CartComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -14,11 +15,11 @@ import {CartComponent} from './cart/cart.component';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'products-details', component: ProductListComponent },
       { path: 'cart', component: CartComponent },
     ])
   ],
@@ -27,13 +28,11 @@ import {CartComponent} from './cart/cart.component';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
   ],
   bootstrap: [
     AppComponent
-  ],
-  providers: [
-    CartService
   ]
 })
 export class AppModule { }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,11 +6,9 @@ import { CartService } from '../cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent {
   items = this.cartService.getItems();
   constructor(private cartService: CartService) { }
 
-  ngOnInit() {
-  }
 
 }
